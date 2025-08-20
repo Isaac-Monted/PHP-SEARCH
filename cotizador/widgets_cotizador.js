a6f3b21220d596e8d43f4d68bfe1d2b449a1232a
EdniMorales <rimora.29@gmail.com>
2025-08-20 14:42:25 -0600

    CAMBIOS PENDEJOS 2

--- cotizador/widgets_cotizador.js
+++ cotizador/widgets_cotizador.js
@@ -22,6 +22,8 @@ export function ColocarElCarritoEnLaPagina(descripcion){
 
         const productCard = document.createElement("div");
         productCard.classList.add("container");
+        productCard.classList.add("stilo-carrito");
+        productCard.classList.add("custom-btn-sm");
 
         // Agregar contenido a la tarjeta del producto
         productCard.innerHTML = `
@@ -35,18 +37,30 @@ export function ColocarElCarritoEnLaPagina(descripcion){
                     <div>
                         <a href="../articulo/articulo.html?id=${Articulos.Id}"><p>${Articulos.Nombre}</p></a>
                     </div>
-                    <div>
-                        <input class="casillaNumeros" type="text" id="Cantidad${Articulos.Id}" name="Cantidad${Articulos.Id}" value=${Articulos.Cantidad} style="text-align: center;" disabled>
-                        <button id="BotonDecremento${Articulos.Id}"> - </button>
-                        <button id="BotonIncremento${Articulos.Id}"> + </button>
-                    </div>
-                    <div>
 
+
+                    <div class="container">
+                    <div class="row col-lg-12 mb-3">
+
+
+                    <div class="col-12 col-md-4">
+                        <input class="casillaNumeros col-lg-4" type="text" id="Cantidad${Articulos.Id}" name="Cantidad${Articulos.Id}" value=${Articulos.Cantidad} style="text-align: center;" disabled>
                     </div>
+
+
+                    <div class="btn-group col-4" role="group" aria-label="Basic example">
+                        <button class="custom-btn-sm" id="BotonDecremento${Articulos.Id}"> - </button>
+                        <button class="btn btn-primary" id="BotonIncremento${Articulos.Id}"> + </button>
+                    </div>
+                    </div>
+                    </div>
+
+
+
                 </div>
                 <div class="right-column"> <!-- Columna derecha -->
                     <div>
-                        <button id="BotonQuitar${Articulos.Id}"> Quitar </button>
+                        <button class="btn btn-primary" id="BotonQuitar${Articulos.Id}"> Quitar </button>
                     </div>
                 </div>
             </div>
